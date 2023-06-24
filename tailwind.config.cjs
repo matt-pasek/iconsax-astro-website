ac27c052f581482eb9881a42b61ab8d12c925190
matt-pasek <mpasek@tm1.edu.pl>
2023-06-24 23:13:47 +0200

    added prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'catamaran': ['Catamaran', 'sans-serif'],
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        catamaran: ['Catamaran', 'sans-serif'],
+      },
+      colors: {
+        white: '#E5E7E6',
+        black: '#141301',
+        accent: '#9A48D0',
+      },
+    },
+  },
+  plugins: [],
+};
